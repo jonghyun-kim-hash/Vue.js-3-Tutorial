4e43785479ec418fc1e982d5c3c4726d600b0fd7
JH KIM <pjhkim0408@gmail.com>
2023-09-12 23:36:46 +0900

    vue router (part1)

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+      blanditiis! Quas perspiciatis consequatur debitis sit ipsa. Quia ipsa
+      natus obcaecati voluptate. Molestiae recusandae repudiandae, nobis
+      dolorum, dolorem possimus repellendus enim numquam perferendis aliquid,
+      quisquam distinctio vel aut laboriosam aliquam cumque non officia impedit
+      placeat unde quis. Aspernatur tempora aut officia accusamus accusantium
+      fugit nisi maxime, sapiente laudantium fuga. Vel ex possimus animi vero
+      commodi? Excepturi, ipsum. Quo placeat sapiente laudantium necessitatibus
+      veniam quas, pariatur beatae consequuntur alias dolorum eos quisquam quis
+      quia expedita repellat quaerat ea, nesciunt doloribus, voluptate dolores
+      excepturi enim porro. Voluptas, blanditiis temporibus facere obcaecati
+      expedita ab.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+      blanditiis! Quas perspiciatis consequatur debitis sit ipsa. Quia ipsa
+      natus obcaecati voluptate. Molestiae recusandae repudiandae, nobis
+      dolorum, dolorem possimus repellendus enim numquam perferendis aliquid,
+      quisquam distinctio vel aut laboriosam aliquam cumque non officia impedit
+      placeat unde quis. Aspernatur tempora aut officia accusamus accusantium
+      fugit nisi maxime, sapiente laudantium fuga. Vel ex possimus animi vero
+      commodi? Excepturi, ipsum. Quo placeat sapiente laudantium necessitatibus
+      veniam quas, pariatur beatae consequuntur alias dolorum eos quisquam quis
+      quia expedita repellat quaerat ea, nesciunt doloribus, voluptate dolores
+      excepturi enim porro. Voluptas, blanditiis temporibus facere obcaecati
+      expedita ab.
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
