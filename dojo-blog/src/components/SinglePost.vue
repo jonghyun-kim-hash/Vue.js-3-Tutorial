a1eb9d27fff05ef7854b267fe2acfd108e1b2868
JH KIM <pjhkim0408@gmail.com>
2023-09-17 22:47:55 +0900

    props, hooks in setup

---
+++ dojo-blog/src/components/SinglePost.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <h3>{{ post.title }}</h3>
+    <p>{{ snippet }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  props: ["post"],
+  setup(props) {
+    const snippet = computed(() => {
+      const body = props.post.body;
+      return body.length > 100 ? body.substring(0, 100) + "...." : body;
+    });
+    return { snippet };
+  },
+};
+</script>
+
+<style></style>
